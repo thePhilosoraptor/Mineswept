0f852a9f3c1ff7c5b7376361ae56f00585b9361e
thePhilosoraptor <54124658+thePhilosoraptor@users.noreply.github.com>
2019-08-14 19:57:28 -0400

    Add files via upload

---
+++ LogicTile.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogicTile : MonoBehaviour
+{
+    private Tile baseTile;
+    private int numFlaggedAdjacentLTs;
+    private int activeAdjacentMinesModifier;
+
+    public Vector2Int Coordinates { get; set; }
+    public bool IsExpanded { get; private set; }
+    public int NumActiveAdjacentMines { get; private set; }
+    public int ActiveAdjacentMinesModifier
+    {
+        get { return activeAdjacentMinesModifier;}
+        set { if (value >= 0) { activeAdjacentMinesModifier = value; } }
+    }
+    public List<LogicTile> UnexpandedAdjacentLTs { get; set; }
+
+    public int NumAdjacentMines
+    {
+        get { return baseTile.NumAdjacentMines; }
+    }
+    public bool IsRevealed
+    {
+        get { return baseTile.IsRevealed; }
+    }
+    public bool IsFlagged
+    {
+        get { return baseTile.IsFlagged; }
+    }
+
+    
+    public void LogicTileSetup(Tile t, int xCoord, int yCoord)
+    {
+        baseTile = t;
+        Coordinates = new Vector2Int(xCoord, yCoord);
+
+        IsExpanded = false;
+        UnexpandedAdjacentLTs = new List<LogicTile>();
+
+        activeAdjacentMinesModifier = 0;
+        numFlaggedAdjacentLTs = 0;
+        UpdateNumActiveAdjacentMines();
+    }
+
+    public void LogicTileSetup(LogicTile lt)
+    {
+        baseTile = lt.baseTile;
+        Coordinates = lt.Coordinates;
+
+        IsExpanded = lt.IsExpanded;
+        UnexpandedAdjacentLTs = lt.UnexpandedAdjacentLTs;
+
+        activeAdjacentMinesModifier = lt.activeAdjacentMinesModifier;
+        numFlaggedAdjacentLTs = lt.numFlaggedAdjacentLTs;
+        UpdateNumActiveAdjacentMines();
+    }
+
+    public void LeftClick()
+    {
+        baseTile.RevealTileBtn();
+    }
+
+    public void RightClick()
+    {
+        baseTile.ToggleFlaggedBtn();
+    }
+
+    public void Expand()
+    {
+        IsExpanded = true;
+        SetLogicTileColor(Color.red);
+
+        UpdateUnexpandedAdjacentLTs();
+    }
+
+    public void UpdateUnexpandedAdjacentLTs()
+    {
+        foreach (LogicTile adjacent in UnexpandedAdjacentLTs)
+        {
+            if (IsFlagged)
+            {
+                adjacent.numFlaggedAdjacentLTs++;
+            }
+
+            if (IsExpanded)
+            {
+                adjacent.UnexpandedAdjacentLTs.Remove(this);
+            }
+
+            adjacent.UpdateNumActiveAdjacentMines();
+        }
+    }
+
+    public void UpdateNumActiveAdjacentMines()
+    {
+        NumActiveAdjacentMines = NumAdjacentMines - numFlaggedAdjacentLTs - activeAdjacentMinesModifier;
+
+        if (NumActiveAdjacentMines < 0)
+        {
+            NumActiveAdjacentMines = 0;
+        }
+    }
+
+    public List<LogicTile> FindRevealedAdjacentLTs()
+    {
+        List<LogicTile> ruaLTs = new List<LogicTile>();
+
+        foreach (LogicTile adjacent in UnexpandedAdjacentLTs)
+        {
+            if (adjacent.IsRevealed)
+            {
+                ruaLTs.Add(adjacent);
+            }
+        }
+
+        return ruaLTs;
+    }
+
+    public List<LogicTile> FindUnrevealedAdjacentLTs()
+    {
+        List<LogicTile> adjacentLTs = new List<LogicTile>();
+
+        foreach (LogicTile lt in UnexpandedAdjacentLTs)
+        {
+            if (!lt.IsRevealed)
+            {
+                adjacentLTs.Add(lt);
+            }
+        }
+
+        return adjacentLTs;
+    }
+
+    public List<LogicTile> FindUnrevealedIntersect(LogicTile adjacent)
+    {
+        List<LogicTile> intersect = new List<LogicTile>();
+
+        foreach (LogicTile lt in FindUnrevealedAdjacentLTs())
+        {
+            if (adjacent.UnexpandedAdjacentLTs.Contains(lt))
+            {
+                intersect.Add(lt);
+            }
+        }
+
+        return intersect;
+    }
+
+    public void SetLogicTileColor(Color c)
+    {
+        c.a = 0.25f;
+        GetComponent<Image>().color = c;
+    }
+}
